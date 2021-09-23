0baa182e72d225694a3c9aedb2863066da6dd48c
1491224544 <1491224544@qq.com>
2021-09-23 22:32:22 +0800

    perf: 添加数据库建库指令

---
+++ code/server/db.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `message` (
+  `roomid` varchar(16) DEFAULT NULL,
+  `userid` varchar(16) DEFAULT NULL,
+  `content` text,
+  `timestamp` varchar(13) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE `user` (
+  `userid` varchar(16) DEFAULT NULL,
+  `pwd` varchar(32) DEFAULT NULL,
+  `username` text
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
