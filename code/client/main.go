92060a3759c210587f9a65b7212a53a5ff6dc04a
1491224544 <1491224544@qq.com>
2021-09-23 09:25:52 +0800

    feat: 增加数据加密传输，完成基本消息传输框架

--- code/client/main.go
+++ code/client/main.go
@@ -2,10 +2,20 @@ package main
 
 import (
 	"bufio"
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
+	cryprand "crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/base64"
+	"encoding/pem"
 	"errors"
 	"fmt"
 	"log"
+	"math/rand"
 	"net"
+	"os"
 	"strconv"
 	"strings"
 	"time"
@@ -14,19 +24,116 @@ import (
 var id string
 var msgid int
 var msgs = make(map[int]string)
+var roomid int
+var aeskey []byte
 
+// RSA加密
+func RSA_Encrypt(plainText []byte, path string) []byte {
+	file, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	info, _ := file.Stat()
+	buf := make([]byte, info.Size())
+	file.Read(buf)
+	block, _ := pem.Decode(buf)
+
+	publicKeyInterface, err := x509.ParsePKIXPublicKey(block.Bytes)
+	if err != nil {
+		panic(err)
+	}
+	publicKey := publicKeyInterface.(*rsa.PublicKey)
+	cipherText, err := rsa.EncryptPKCS1v15(cryprand.Reader, publicKey, plainText)
+	if err != nil {
+		panic(err)
+	}
+	return cipherText
+}
+
+// AES加密的一部分
+func PKCS5Padding(ciphertext []byte, blockSize int) []byte {
+	padding := blockSize - len(ciphertext)%blockSize
+	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
+	return append(ciphertext, padtext...)
+}
+
+// AES加密的一部分
+func PKCS5UnPadding(origData []byte) []byte {
+	length := len(origData)
+	unpadding := int(origData[length-1])
+	return origData[:(length - unpadding)]
+}
+
+// AES加密
+func AesEncrypt(origData, key []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	blockSize := block.BlockSize()
+	origData = PKCS5Padding(origData, blockSize)
+	blockMode := cipher.NewCBCEncrypter(block, key[:blockSize])
+	crypted := make([]byte, len(origData))
+	blockMode.CryptBlocks(crypted, origData)
+	return crypted, nil
+}
+
+// AES解密
+func AesDecrypt(crypted, key []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	blockSize := block.BlockSize()
+	blockMode := cipher.NewCBCDecrypter(block, key[:blockSize])
+	origData := make([]byte, len(crypted))
+	blockMode.CryptBlocks(origData, crypted)
+	origData = PKCS5UnPadding(origData)
+	return origData, nil
+}
+
+// 获取长度为l的只含数字和小写字母的随机字符串
+func GetRandomString(l int) string {
+	str := "0123456789abcdefghijklmnopqrstuvwxyz"
+	bytes := []byte(str)
+	result := []byte{}
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for i := 0; i < l; i++ {
+		result = append(result, bytes[r.Intn(len(bytes))])
+	}
+	return string(result)
+}
+
+// 从连接读取一次数据并解密
 func readstring(conn net.Conn) (string, error) {
+	// 先进行base64解码后aes解密
 	res, err := bufio.NewReader(conn).ReadString('\n')
 	if err != nil {
 		fmt.Println(err)
 	}
-	return res, err
+
+	bytesPass, err := base64.StdEncoding.DecodeString(res)
+	if err != nil {
+		fmt.Println(err)
+		return "", err
+	}
+
+	finalres, err := AesDecrypt(bytesPass, aeskey)
+	return string(finalres), err
 }
 
+// 向连接发送消息
 func sendmsg(conn net.Conn, info string) (int, error) {
-	return conn.Write([]byte(info + "\n"))
+	// aes加密后base64发送
+	encbyte, _ := AesEncrypt([]byte(info), []byte(aeskey))
+	enctext := base64.StdEncoding.EncodeToString(encbyte)
+	return conn.Write([]byte(enctext + "\n"))
 }
 
+// 监听消息循环
 func listen(conn net.Conn) {
 	for {
 		data, err := readstring(conn)
@@ -35,11 +142,14 @@ func listen(conn net.Conn) {
 		}
 		data = strings.Replace(data, "\n", "", -1)
 		res := strings.Split(data, "|")
+
 		if res[0] == "ret" {
+			// 如果消息头是返回值，则将数据填充至对应序列号的返回值
 			cnt, _ := strconv.ParseInt(res[1], 10, 64)
 			msgs[int(cnt)] = res[2]
 		}
 		if res[0] == "msg" {
+			// 如果消息头是消息，则显示出来
 			if res[1] != id {
 				fmt.Println(res[1], strings.Replace(res[2], "{{shu}}", "|", -1))
 			}
@@ -47,15 +157,18 @@ func listen(conn net.Conn) {
 	}
 }
 
+// 发送一个请求
 func sendreq(conn net.Conn, opt string, msg string) (string, error) {
+	// 为请求分配序列号，同时等待返回值被填充
 	msgid++
 	msgs[msgid] = ""
 	strid := fmt.Sprintf("%d", msgid)
-	_, err := conn.Write([]byte(opt + "|" + strid + "|" + msg + "\n"))
+	_, err := sendmsg(conn, opt+"|"+strid+"|"+msg)
 	if err != nil {
 		return "", err
 	}
 
+	// 循环等待请求返回
 	for i := 0; i < 1000; i++ {
 		if msgs[msgid] != "" {
 			return msgs[msgid], err
@@ -67,13 +180,29 @@ func sendreq(conn net.Conn, opt string, msg string) (string, error) {
 
 func main() {
 	conn, err := net.Dial("tcp", ":2300")
-	msgid = 0
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer conn.Close()
+	msgid = 0
+	roomid = 0
+	// 生成aeskey
+	aeskey = []byte(GetRandomString(16))
+	cipherText := base64.StdEncoding.EncodeToString(RSA_Encrypt(aeskey, "public.pem"))
 	var data string
+	// 发送aeskey并等待返回
+	for {
+		conn.Write([]byte("aeskey|" + cipherText + "\n"))
+		if r, _ := readstring(conn); r == "success" {
+			break
+		} else {
+			fmt.Println(r)
+		}
+	}
+
+	// 启动监听循环
 	go listen(conn)
+	// 注册id
 	for {
 		fmt.Print("请输入您的userid:")
 		fmt.Scanln(&id)
@@ -88,6 +217,7 @@ func main() {
 			fmt.Print(ret, "请输入您的userid:")
 		}
 	}
+	// 处理用户输入
 	for {
 		fmt.Scanln(&data)
 		data = strings.Replace(data, "|", "{{shu}}", -1)
--- code/server/main.go
+++ code/server/main.go
@@ -6,25 +6,201 @@ import (
 	"log"
 	"net"
 	"strings"
+	"time"
+
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/base64"
+	"encoding/pem"
+	"os"
+
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
 )
 
+// 生成RSA私钥和公钥，保存到文件中
+func GenerateRSAKey(bits int) {
+	privateKey, err := rsa.GenerateKey(rand.Reader, bits)
+	if err != nil {
+		panic(err)
+	}
+	X509PrivateKey := x509.MarshalPKCS1PrivateKey(privateKey)
+	privateFile, err := os.Create("private.pem")
+	if err != nil {
+		panic(err)
+	}
+	defer privateFile.Close()
+	privateBlock := pem.Block{Type: "RSA Private Key", Bytes: X509PrivateKey}
+	pem.Encode(privateFile, &privateBlock)
+
+	publicKey := privateKey.PublicKey
+	X509PublicKey, err := x509.MarshalPKIXPublicKey(&publicKey)
+	if err != nil {
+		panic(err)
+	}
+	publicFile, err := os.Create("public.pem")
+	if err != nil {
+		panic(err)
+	}
+	defer publicFile.Close()
+	publicBlock := pem.Block{Type: "RSA Public Key", Bytes: X509PublicKey}
+	pem.Encode(publicFile, &publicBlock)
+}
+
+// RSA加密
+func RSA_Encrypt(plainText []byte, path string) []byte {
+	file, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	info, _ := file.Stat()
+	buf := make([]byte, info.Size())
+	file.Read(buf)
+	block, _ := pem.Decode(buf)
+
+	publicKeyInterface, err := x509.ParsePKIXPublicKey(block.Bytes)
+	if err != nil {
+		panic(err)
+	}
+	publicKey := publicKeyInterface.(*rsa.PublicKey)
+	cipherText, err := rsa.EncryptPKCS1v15(rand.Reader, publicKey, plainText)
+	if err != nil {
+		panic(err)
+	}
+	return cipherText
+}
+
+// RSA解密
+func RSA_Decrypt(cipherText []byte, path string) []byte {
+	file, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	info, _ := file.Stat()
+	buf := make([]byte, info.Size())
+	file.Read(buf)
+	block, _ := pem.Decode(buf)
+	privateKey, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+	if err != nil {
+		panic(err)
+	}
+	plainText, _ := rsa.DecryptPKCS1v15(rand.Reader, privateKey, cipherText)
+	return plainText
+}
+
+// AES加密的一部分
+func PKCS5Padding(ciphertext []byte, blockSize int) []byte {
+	padding := blockSize - len(ciphertext)%blockSize
+	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
+	return append(ciphertext, padtext...)
+}
+
+// AES加密的一部分
+func PKCS5UnPadding(origData []byte) []byte {
+	length := len(origData)
+	unpadding := int(origData[length-1])
+	return origData[:(length - unpadding)]
+}
+
+// AES加密
+func AesEncrypt(origData, key []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	blockSize := block.BlockSize()
+	origData = PKCS5Padding(origData, blockSize)
+	blockMode := cipher.NewCBCEncrypter(block, key[:blockSize])
+	crypted := make([]byte, len(origData))
+	blockMode.CryptBlocks(crypted, origData)
+	return crypted, nil
+}
+
+// AES解密
+func AesDecrypt(crypted, key []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	blockSize := block.BlockSize()
+	blockMode := cipher.NewCBCDecrypter(block, key[:blockSize])
+	origData := make([]byte, len(crypted))
+	blockMode.CryptBlocks(origData, crypted)
+	origData = PKCS5UnPadding(origData)
+	return origData, nil
+}
+
+// 连接池
 var conns = make(map[string]net.Conn)
+
+// 在线用户id
 var onlineusers = make(map[string]string)
+
+// 链接ip获取id
 var iptoid = make(map[string]string)
 
+// AES密钥池
+var aeskeys = make(map[string]string)
+
+// 数据库连接对象
+var Db *sqlx.DB
+
+// 数据库配置
+var (
+	userName  string = "pschat"
+	password  string = "pschat2021"
+	ipAddrees string = "127.0.0.1"
+	port      int    = 3306
+	dbName    string = "pschat"
+	charset   string = "utf8"
+)
+
+// 连接sql数据库
+func connectMysql() *sqlx.DB {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=%s", userName, password, ipAddrees, port, dbName, charset)
+	Db, err := sqlx.Open("mysql", dsn)
+	if err != nil {
+		fmt.Printf("mysql connect failed, detail is [%v]", err.Error())
+	}
+	return Db
+}
+
+// int转string
+func itos(i int) string {
+	strid := fmt.Sprintf("%d", i)
+	return strid
+}
+
+// 广播消息
 func sendboard(msg string) {
+	// 从在线用户中循环，向每个在线用户发布信息
 	for _, addr := range onlineusers {
 		if addr != "" {
 			conn := conns[addr]
-			sendmsg(conn, msg)
+			aeskey := aeskeys[conn.RemoteAddr().String()]
+			sendmsg(conn, msg, aeskey)
 		}
 	}
 }
 
-func sendmsg(conn net.Conn, info string) (int, error) {
-	return conn.Write([]byte(info + "\n"))
+// 发送消息
+func sendmsg(conn net.Conn, info string, aeskey string) (int, error) {
+	// 对消息进行aes加密后base64，传输密文
+	encbyte, _ := AesEncrypt([]byte(info), []byte(aeskey))
+	enctext := base64.StdEncoding.EncodeToString(encbyte)
+	return conn.Write([]byte(enctext + "\n"))
 }
 
+// 从连接读取字符串至换行符
 func readstring(conn net.Conn) (string, error) {
 	res, err := bufio.NewReader(conn).ReadString('\n')
 	if err != nil {
@@ -33,7 +209,18 @@ func readstring(conn net.Conn) (string, error) {
 	return res, err
 }
 
+// 添加消息记录到sql
+func addmsgrecord(roomid, userid, msg string) {
+	_, err := Db.Exec("insert into message (roomid,userid,content,timestamp) values(?,?,?,?)", roomid, userid, msg, itos(int(time.Now().UnixMilli())))
+	if err != nil {
+		fmt.Printf("data insert faied, error:[%v]", err.Error())
+		return
+	}
+}
+
+// 注册用户
 func reguser(conn net.Conn, userid string) string {
+	// 如果id未被占用或者占用者为空，则允许注册
 	if id, ok := onlineusers[userid]; ok && id != "" {
 		return "error"
 	}
@@ -43,27 +230,57 @@ func reguser(conn net.Conn, userid string) string {
 
 }
 
+// 处理连接，消息循环
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
+	aeskey := ""
+
 	for {
+
 		data, err := readstring(conn)
 		if err != nil {
 			onlineusers[iptoid[conn.RemoteAddr().String()]] = ""
 			break
 		}
+
 		data = strings.Replace(data, "\n", "", -1)
-		fmt.Println(conn.RemoteAddr().String(), data)
 		res := strings.Split(data, "|")
-		if res[0] == "reg" {
-			sendmsg(conn, "ret|"+res[1]+"|"+reguser(conn, res[2]))
-		}
-		if res[0] == "msg" {
-			sendboard("msg|" + iptoid[conn.RemoteAddr().String()] + "|" + res[1])
+
+		if len(res) > 1 {
+			if res[0] == "aeskey" {
+				// 如果传来信息为aeskey，则不进行解密
+				aeskeybyte, _ := base64.StdEncoding.DecodeString(res[1])
+				aeskey = string(RSA_Decrypt(aeskeybyte, "private.pem"))
+				aeskeys[conn.RemoteAddr().String()] = aeskey
+				fmt.Println(conn.RemoteAddr().String(), "aeskey", aeskey)
+				sendmsg(conn, "success", aeskey)
+			}
+		} else {
+			// 否则解密后进行解析
+			bytesPass, _ := base64.StdEncoding.DecodeString(data)
+			databyte, _ := AesDecrypt(bytesPass, []byte(aeskey))
+			data = string(databyte)
+
+			data = strings.Replace(data, "\n", "", -1)
+			fmt.Println(conn.RemoteAddr().String(), data)
+
+			res := strings.Split(data, "|")
+			if res[0] == "reg" {
+				// 注册消息
+				sendmsg(conn, "ret|"+res[1]+"|"+reguser(conn, res[2]), aeskey)
+			}
+			if res[0] == "msg" {
+				// 发送消息
+				addmsgrecord(itos(0), iptoid[conn.RemoteAddr().String()], res[1])
+				sendboard("msg|" + iptoid[conn.RemoteAddr().String()] + "|" + res[1])
+			}
 		}
 	}
 }
 
 func main() {
+	Db = connectMysql()
+	defer Db.Close()
 	listener, err := net.Listen("tcp", ":2300")
 	if err != nil {
 		log.Fatal(err)
